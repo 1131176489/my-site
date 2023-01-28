2eda90885b9b55d73e39175a673a7197eb970eac
陈刚 <1131176489@qq.com>
2023-01-29 03:37:14 +0800

    登录组件一部分布局

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 
 <template>
-    <Home>
-
-    </Home>
+    <component :is=currentComponent[key]></component>
 </template>
 <script setup lang="ts">
-import Home from "./components/Home.vue";
+import Home from "./components/Home.vue"
+import Login from "./components/Login.vue"
+const key = 0
+const currentComponent = [Login, Home,]
 </script>
 <style scoped>
 
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,19 +1,31 @@
 <template>
-    <div class="login">
-        <el-input v-model="username" placeholder="Please input" />
-        <el-input v-model="password" placeholder="Please input" />
-        <el-input v-model="email" placeholder="Please input" />
-        
+    <div class="container">
+        <div class="login">
+            <el-input v-model="username" placeholder="Please input" />
+            <el-input v-model="password" placeholder="Please input" />
+            <el-input v-model="email" placeholder="Please input" />
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref } from "vue"
 const username = ref("")
 const password = ref("")
 const email = ref("")
 </script>
 
 <style>
+.container {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 
+.login {
+    width: 500px;
+    height: 500px;
+}
 </style>
