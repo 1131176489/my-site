8b4ab01cf622d9d171243972cfc088e9c235ad4e
陈刚 <1131176489@qq.com>
2023-01-29 03:24:14 +0800

    hu

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,11 +3,12 @@
         <el-input v-model="username" placeholder="Please input" />
         <el-input v-model="password" placeholder="Please input" />
         <el-input v-model="email" placeholder="Please input" />
+        
     </div>
 </template>
 
-<script lang="ts">
-import { ref } from 'vue';
+<script lang="ts" setup>
+import { ref } from "vue";
 const username = ref("")
 const password = ref("")
 const email = ref("")
