1e2a3f77304c13df3d0e5531b655c33bff55355e
陈刚 <1131176489@qq.com>
2023-01-30 04:23:34 +0800

    创建注册组件

---
+++ src/components/Register.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="login">
+            <label for="cheese">Do you like cheese?</label>
+            <el-input v-model="email" placeholder="请输入电子邮件" />
+            <el-input v-model="password" placeholder="Please input" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue"
+const password = ref("")
+const email = ref("")
+</script>
+
+<style scoped>
+.container {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.login {
+    width: 250px;
+    height: 200px;
+}
+label{
+    font-size: 16px;
+}
+</style>
