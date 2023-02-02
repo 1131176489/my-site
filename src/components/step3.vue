daf8af0886636de5d02695fb22e3fb0354985cff
陈刚 <1131176489@qq.com>
2023-02-02 20:52:41 +0800

    注册第三步

---
+++ src/components/step3.vue
@@ -0,0 +1,31 @@
+<template>
+        <div>
+                注册成功！
+        </div>
+        <el-button type="primary" style="width: 400px;" v-on:click="handleclick">
+                确定
+        </el-button>   
+</template>
+<script lang="ts" setup>
+import { onMounted } from "vue"
+import { useRouter } from "vue-router"
+import { registerStore } from "../assets/register"
+const registerData = registerStore()
+let { set_step } = registerData
+onMounted(()=>{
+        set_step(3)
+}) 
+const router = useRouter()
+const handleclick = () => {
+        console.log(123)
+        router.push({
+                name:"Login"
+        })
+}
+</script>
+
+<style>
+.el-form {
+        height: 210px;
+}
+</style>
