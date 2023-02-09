bb7a1ed9b062f2386e98bed38bc847c87290205a
陈刚 <1131176489@qq.com>
2023-02-10 01:28:44 +0800

    删除多余的注释和空格

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -9,9 +9,7 @@ const treeRef = ref<InstanceType<typeof ElTree>>()
 //从pin2.ts引入数据
 const videoData = myStore()
 const vi = ref<HTMLVideoElement | null>(null)
-
 const timer = ref(0)
-
 const defaultProps = {
         children: 'children',
         label: 'label',
@@ -93,9 +91,8 @@ function lastVideo(info: string): void {
 }
 //组件挂载后的处理函数
 const pageBegin = async function () {
-
         if (import.meta.env.MODE == 'development') {
-                videoData.srcBaseUrl = "http://192.168.0.106:9000/"+"qq666"
+                videoData.srcBaseUrl = "http://192.168.0.106:9000/"
         }
         else {
                 videoData.srcBaseUrl = "/"
@@ -106,7 +103,6 @@ const pageBegin = async function () {
         const { data }: { data: lastVideo } = await axios.get("/lastVideo")
         console.log(data)
         videoData.playInfo = data.msg
-
         //将当前高亮设置为播放信息数据包的标签
         videoData.currentHeightLight = videoData.playInfo.label
         //设置当前视频源url
@@ -120,24 +116,14 @@ const pageBegin = async function () {
         //将视频进度前进到播放信息数据包的时间位置，将音量设置为0.6，播放视频
         setTimeout(() => {
                 vi.value!.volume = 0.6
-
                 vi.value!.currentTime = parseInt(videoData.playInfo.time) - 2
-
                 vi.value?.play()
         }, 1500)
         //设置Container容器高度
         reSizeContainer()
 }
-// //高亮处理函数
-// function handleHeightLight(target: Event) {
-
-// }
 //点击视频标题节点的回调函数
 function handleClick(f1: Tree, f2: any, f3: any, f4: any): void {
-        console.log(f1)
-        console.log(f2)
-        console.log(f3)
-        console.log(f4)
         //获取currentHeightLight类名的元素
         const d = window.document.documentElement.getElementsByClassName('currentHeightLight')
         if (d.length != 0) {
@@ -176,7 +162,6 @@ function videoEnd(info: number) {
         if (info == 1) videoData.index++
         //否则，则当前视频文件所在父文件中的位置-1，
         else videoData.index--
-
         //如果当前视频文件所在父文件中的位置大于（当前播放列表的长度-1），则视频文件所在父文件中的位置为0，
         if (videoData.index > videoData.currentPlayList.children!.length - 1) {
                 videoData.index = 0
@@ -191,7 +176,6 @@ function videoEnd(info: number) {
         videoData.src = videoData.srcBaseUrl + videoData.currentPlayList.children![videoData.index].label
         //播放视频
         vi.value?.play()
-
 }
 //添加至播放列表
 async function playListAdd() {
@@ -247,7 +231,6 @@ async function playListAdd() {
 async function playListClear() {
         const res = await axios.post('/palyListClear')
         ff()
-
 }
 //按键上一个视频的回调
 function preVideo() {
@@ -267,7 +250,6 @@ onMounted(() => {
 })
 window.onpageshow = pageBegin
 onBeforeUnmount(() => {
-        // 
         window.clearInterval(timer.value)
         lastVideo('onBeforeUnmount')
 })
@@ -278,7 +260,6 @@ window.onpagehide = () => {
         lastVideo('onpagehide')
 }
 window.onresize = reSizeContainer
-
 </script>
 
 <template>
@@ -292,12 +273,9 @@ window.onresize = reSizeContainer
                                ref="vi"></video>
                 </div>
                 <el-scrollbar>
-
                         <el-tree :data="videoData.mydata" v-bind:render-after-expand=true
                                  v-bind:props="defaultProps"
                                  v-on:node-click="handleClick" ref="treeRef" />
-                        <!-- <div class="video-list">
-                        </div> -->
                 </el-scrollbar>
                 <el-card class="box-card">
                         <el-button v-on:click="playListAdd">
@@ -313,30 +291,22 @@ window.onresize = reSizeContainer
                                 <el-button v-on:click="nextVideo">
                                         下一个视频
                                 </el-button>
-
                         </div>
                 </el-card>
-                <div >
-
+                <div>
                 </div>
-
         </div>
-
-
 </template>
 <style scoped >
 @media screen and (min-width:765px) {
         .currentHeightLight {
                 background-color: #337ecc !important;
         }
-
         .container {
                 display: flex;
                 height: 100%;
                 justify-content: left;
-                /* overflow: scroll; */
         }
-
         video {
                 display: block;
                 width: 1044px;
@@ -345,25 +315,18 @@ window.onresize = reSizeContainer
                 padding: 0;
                 background-color: #000;
         }
-
         .video-container {
                 width: 1044px;
                 margin-left: 150px;
                 flex-shrink: 0;
         }
-
         .video-title {
-
                 height: 60px;
                 padding-top: 20px;
                 font-size: 16px;
                 font-family: 'Times New Roman', Times, serif;
         }
-
-
-
         .video-list {
-                /* flex: 1; */
                 flex-shrink: 0;
                 overflow: scroll;
                 width: 300px;
