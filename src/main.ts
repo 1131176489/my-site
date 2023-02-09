e3de8f07e62c44feee72054ef88bd1e22b65a1e2
陈刚 <1131176489@qq.com>
2023-02-10 00:24:31 +0800

    导航守卫

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import axios from "axios"
 import * as ElementPlusIconsVue from "@element-plus/icons-vue"
 import rem from "./plugins/rem"
 import reSizeContainer from "./plugins/reSizeContainer"
+import { ElMessage } from "element-plus"
 
 const pinia = createPinia()
 const app = createApp(App)
@@ -17,10 +18,47 @@ app.provide("reSizeContainer", reSizeContainer)
 app.config.globalProperties.$rem()
 axios.defaults.baseURL = "http://192.168.0.106:9000"
 axios.defaults.timeout = 6000
+axios.interceptors.response
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
 app.use(router)
 
+
+router.beforeEach(async (to, from) => {
+        let isAuthenticated = localStorage.getItem("token")
+        if (!isAuthenticated) {
+                if (
+                        to.name !== "Login" &&
+                        to.name !== "step1"&&
+                        to.name !== "step2"&&
+                        to.name !== "step3")
+                {
+                                return { name: "Login" }
+                }
+        }
+        else {
+                axios.post("./test", null, {
+                        headers: {
+                                'Authorization': "Bearer " + localStorage.getItem("token"),
+                        },
+                }).then(() => {
+                        router.push({
+                                name: "Home"
+                        })
+                }).catch(() => {
+                        ElMessage({
+                                type: "warning",
+                                message: "登陆失效，请重新登录"
+                        })
+                        localStorage.removeItem("token")
+                        router.push({
+                                name: "Login"
+                        })
+                })
+                axios.defaults.headers.Authorization = "Bearer " + isAuthenticated
+                // axios.defaults.headers.Authorization = null
+        }
+    })
 app.mount("#app")  
