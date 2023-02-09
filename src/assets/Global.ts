dc9e783a95db5f417b2fdb54bf38bbdc76c4f103
陈刚 <1131176489@qq.com>
2023-02-10 00:23:58 +0800

    全局变量

---
+++ src/assets/Global.ts
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const globalStore = defineStore('global', () =>{
+        const isAuthenticated = ref("")
+        const set_isAuthenticated = (p:string)=>{
+                isAuthenticated.value = p
+        }
+        const get_isAuthenticated = ()=>{
+              return isAuthenticated.value
+      }
+        return {
+                set_isAuthenticated,
+                get_isAuthenticated
+        }
+})
