7ce1adc59826a1a55f468356e45dfaf8a673746f
1131176489 <1131176489@qq.com>
2023-11-26 13:43:34 +0800

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,44 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 import ElementPlus from 'unplugin-element-plus/vite'
 import path from 'path'
-import { resolve } from 'path'
+import {resolve} from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-        build: {
-                minify: 'terser',
-                terserOptions: {
-                        compress: {
-                                drop_console: true,
-                                drop_debugger: true,
-                        },
-                },
-                rollupOptions: {
-                        input: {
-                                main: resolve(__dirname, 'index.html'),
-                                video: resolve(__dirname, 'video/video.html'),
-                        },
-                },
+    build: {
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+            },
         },
-        server: {
-                host: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                video: resolve(__dirname, 'video/video.html'),
+            },
         },
-        resolve: {
-                alias: {
-                        '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-                }
-        },
-        plugins: [
-                vue(),
-                ElementPlus({
-                }),
-                AutoImport({
-                        resolvers: [ElementPlusResolver()],
-                }),
-                Components({
-                        resolvers: [ElementPlusResolver()],
-                }),
-        ]
+    },
+    server: {
+        host: true,
+    },
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
+    plugins: [
+        vue(),
+        ElementPlus({}),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+    ]
 })
